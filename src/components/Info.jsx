a2ad41ce1789ebb53ecce5efb41d7675661dd712
anvoytsehovsky <anvoytsehovsky@gmail.com>
2019-03-27 18:43:20 +0200

    adding components: info, form, weather

---
+++ src/components/Info.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Узнайте погоду в вашем городе</h2>
+      </div>
+    );
+  }
+}
+
+export default Info;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Weather extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Компоннт с погодной информациеей</h2>
+      </div>
+    );
+  }
+}
+
+export default Weather;
